2f13573cac4a9d968b67aeb2ac68e8f09d131869
LinearParadox <amartsinkovskiy@ucsd.edu>
2022-04-14 19:25:30 -0700

    Fixed previous issue.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,26 +8,25 @@ import java.util.ArrayList;
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-        }
-
-        return toReturn;
+	    ArrayList<String> toReturn = new ArrayList<>();
+	    // find the next [, then find the ], then find the (, then read link upto next )
+	    int currentIndex = markdown.indexOf("[");;
+	    while (currentIndex > -1 && currentIndex+1 < markdown.length()) {
+			    int openBracket = markdown.indexOf("[", currentIndex);
+			    int closeBracket = markdown.indexOf("]", openBracket);
+			    int openParen = markdown.indexOf("(", closeBracket);
+			    int closeParen = markdown.indexOf(")", openParen);
+			    toReturn.add(markdown.substring(openParen + 1, closeParen));
+			    currentIndex = markdown.indexOf("[", closeParen);
+		    }
+		
+		    return toReturn;
+	    }
+	
+	    public static void main (String[]args) throws IOException {
+		    Path fileName = Path.of(args[0]);
+		    String content = Files.readString(fileName);
+		    ArrayList<String> links = getLinks(content);
+		    System.out.println(links);
+	    }
     }
-
-
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
-    }
-}
