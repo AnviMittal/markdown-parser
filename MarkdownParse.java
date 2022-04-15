acba3805339a533d0144f8696661375e41ebdfbb
LinearParadox <amartsinkovskiy@ucsd.edu>
2022-04-14 19:40:07 -0700

    Previous issue fixed!

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,17 +10,19 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
 	    ArrayList<String> toReturn = new ArrayList<>();
 	    // find the next [, then find the ], then find the (, then read link upto next )
-	    int currentIndex = markdown.indexOf("[");;
-	    while (currentIndex > -1 && currentIndex+1 < markdown.length()) {
-			    int openBracket = markdown.indexOf("[", currentIndex);
-			    int closeBracket = markdown.indexOf("]", openBracket);
-			    int openParen = markdown.indexOf("(", closeBracket);
-			    int closeParen = markdown.indexOf(")", openParen);
-			    currentIndex = markdown.indexOf("[", closeParen);
+	    int currentIndex = markdown.indexOf("[");
+	    while (currentIndex > -1 && currentIndex + 1 < markdown.length()) {
+		    int openBracket = markdown.indexOf("[", currentIndex);
+		    int closeBracket = markdown.indexOf("]", openBracket);
+		    int openParen = markdown.indexOf("(", closeBracket);
+		    int closeParen = markdown.indexOf(")", openParen);
+		    if (openBracket == 0 || markdown.substring(openBracket - 1, openBracket).compareTo("!") != 0) {
+			    toReturn.add(markdown.substring(openParen + 1, closeParen));
 		    }
-		
-		    return toReturn;
+		    currentIndex = markdown.indexOf("[", closeParen);
 	    }
+			return toReturn;
+    }
 	
 	    public static void main (String[]args) throws IOException {
 		    Path fileName = Path.of(args[0]);
