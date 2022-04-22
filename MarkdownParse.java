56f13c6d74816862eb86da9dc0c2ead99555a4ba
LinearParadox <amartsinkovskiy@ucsd.edu>
2022-04-21 18:43:12 -0700

    Junit!

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
+	
 
     public static ArrayList<String> getLinks(String markdown) {
 	    ArrayList<String> toReturn = new ArrayList<>();
@@ -15,14 +16,24 @@ public class MarkdownParse {
 		    int openBracket = markdown.indexOf("[", currentIndex);
 		    int closeBracket = markdown.indexOf("]", openBracket);
 		    int openParen = markdown.indexOf("(", closeBracket);
+		    closeBracket = trueClose(openParen, markdown, closeBracket);
 		    int closeParen = markdown.indexOf(")", openParen);
 		    if (openBracket == 0 || markdown.substring(openBracket - 1, openBracket).compareTo("!") != 0) {
 			    toReturn.add(markdown.substring(openParen + 1, closeParen));
 		    }
+			
+			
 		    currentIndex = markdown.indexOf("[", closeParen);
 	    }
 			return toReturn;
     }
+	public static int trueClose(int open_paren, String markdown, int close_brack){
+		
+		while(close_brack < markdown.indexOf("(", close_brack)){
+			close_brack = markdown.indexOf("]", close_brack);
+		}
+		return close_brack;
+	}
 	
 	    public static void main (String[]args) throws IOException {
 		    Path fileName = Path.of(args[0]);
