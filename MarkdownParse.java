2ff01621d320dc02af0710843222a154428747ae
AnviMittal <a2mittal@ucsd.edu>
2022-04-24 19:24:46 -0700

    create markdown parse

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,12 +1,9 @@
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
@@ -44,6 +41,7 @@ public class MarkdownParse {
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
 
-        // System.out.println(Character.isLetter('['));
+        
     }
+    
 }
