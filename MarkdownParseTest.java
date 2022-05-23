36b74df16267346d90c766de8c74d23f4f4843fd
Steven Hsu <sthsu@ucsd.edu>
2022-05-22 18:00:50 -0700

    report4 tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,26 @@
 import static org.junit.Assert.*;
+import java.util.*;
 import org.junit.*;
+
 public class MarkdownParseTest {
 	@Test
-	public void addition() {
-		assertEquals(2, 1 + 1);
+	public void snippet1() {
+		String content = Files.readString(Path.of("test-file.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("https://something.com", "some-thing.html"));
+
+        assertArrayEquals(result.toArray(), links.toArray());
 	}
+	@Test
+	public void snippet2() {
+
+	}
+	@Test
+	public void snippet3() {
+
+	}
+
+
 }
 
