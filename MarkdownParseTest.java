55b88478463d475ec4c2d521337f96f7f65f125f
AnviMittal <a2mittal@ucsd.edu>
2022-05-23 11:15:55 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -156,4 +156,31 @@ public class MarkdownParseTest {
 
         assertArrayEquals(result.toArray(), links.toArray());
     }
+
+    @Test
+    public void snipett1() throws IOException{
+        String content = Files.readString(Path.of("./test-file9.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("`google.com", "google.com", "ucsd.edu"));
+        assertArrayEquals(result.toArray(), links.toArray());
+    }
+
+    @Test
+    public void snipett2() throws IOException{
+        String content = Files.readString(Path.of("./test-file10.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("a.com", "a.com(())", "example.com"));
+        assertArrayEquals(result.toArray(), links.toArray());
+
+    }
+    @Test
+    public void snipett3() throws IOException{
+        String content = Files.readString(Path.of("./test-file11.md"));
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        ArrayList<String> result = new ArrayList<>(Arrays.asList("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"));
+        assertArrayEquals(result.toArray(), links.toArray());
+    }
 }
